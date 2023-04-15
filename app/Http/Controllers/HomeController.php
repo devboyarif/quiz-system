553779097e0d847762f5f74df315714b508892c0
Ariful Islam Arif <devboyarif@gmail.com>
2023-04-15 17:56:41 +0600

    showing quiz for registered users

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Quiz;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
 
@@ -9,6 +10,17 @@ class HomeController extends Controller
 {
     public function index(): View
     {
-        return view('home');
+        $query = Quiz::withCount(['questions'])
+            ->has('questions')
+            ->when(auth()->guest() || ! auth()->user()->is_admin, function($query) {
+                return $query->where('published', 1);
+            })
+            ->orderBy('id')
+            ->get();
+
+        $public = $query->where('public', true);
+        $registered = $query->where('public', false);
+
+        return view('home', compact('public', 'registered'));
     }
 }
